f60b7777c586cb2275ad3abb1f502554982464eb
nidnogg <henriquevt98@gmail.com>
2019-09-06 03:26:30 -0300

    Clock useInterval() function, css

--- src/App.js
+++ src/App.js
@@ -51,19 +51,48 @@ const App = () => {
 
 const Clock = () => {
 
-  let d = new Date();
-  let initial_time = d.getHours() + '<span className="blink">:</span>' + d.getMinutes();
-  initial_time = initial_time.split(':').join('<span className="blink">:</span>');
+  let initial_time = getTime();
+ //initial_time = initial_time.split(':').join('<span className="blink">:</span>');
   const [curTime, updateTime] = useState(initial_time);
 
-  useEffect(() => {
-    let d = new Date();
-    updateTime(d.getHours() + '<span className="blink">:</span>' + d.getMinutes());
-  });
+  useInterval(() => {
+    updateTime(getTime());
+  }, 1000);
 
   return ( 
     <h1>{curTime}</h1>
   );
+}
+
+const getTime = () => {
+  let d = new Date();
+
+  let hours = d.getHours() < 10 ? hours = '0' + d.getHours() : d.getHours();
+  let minutes = d.getMinutes() < 10 ? minutes = '0' + d.getMinutes() : d.getMinutes();
+  
+  const curTime = hours + ':' + minutes;
+
+  return curTime;
+}
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
 }
 
 const AudioElem = sourceUrl => {
